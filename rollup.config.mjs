d24e503e738e12ad0895d131b2bf177c8e9f299c
Anastasiia <anufrievaad@txix.ru>
2025-01-17 08:32:39 +0300

    MSP-4704 move to library

---
+++ rollup.config.mjs
@@ -0,0 +1,50 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import image from '@rollup/plugin-image';
+import { babel } from '@rollup/plugin-babel';
+import del from 'rollup-plugin-delete';
+import alias from '@rollup/plugin-alias';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'esm',
+      inlineDynamicImports: true,
+    },
+    plugins: [
+      alias({
+        entries: [
+          { find: '@', replacement: './src' },
+        ],
+      }),
+      del({ targets: 'dist/*' }),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+      }),
+      peerDepsExternal(),
+      resolve({
+        extensions: ['.js', '.jsx'],
+      }),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss({ minimize: true }),
+      terser({
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+        output: {
+          comments: false,
+        },
+      }),
+      image(),
+    ],
+  },
+];
