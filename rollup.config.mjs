bbfed3d5a85b2051a6cdc2ed7c36a1074795db68
foxxy <veselovnk@txix.ru>
2025-01-17 16:36:10 +0300

    MSP-4704 ts build rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,15 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
-import postcss from 'rollup-plugin-postcss';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import image from '@rollup/plugin-image';
-import { babel } from '@rollup/plugin-babel';
-import del from 'rollup-plugin-delete';
-import alias from '@rollup/plugin-alias';
+import alias from '@rollup/plugin-alias'
+import { babel } from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import image from '@rollup/plugin-image'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import del from 'rollup-plugin-delete'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+import postcss from 'rollup-plugin-postcss'
 
 export default [
   {
@@ -19,32 +21,34 @@ export default [
     },
     plugins: [
       alias({
-        entries: [
-          { find: '@', replacement: './src' },
-        ],
+        entries: [{ find: '@', replacement: './src' }]
       }),
       del({ targets: 'dist/*' }),
       babel({
         babelHelpers: 'bundled',
-        exclude: 'node_modules/**',
+        exclude: 'node_modules/**'
       }),
       peerDepsExternal(),
       resolve({
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx']
       }),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        include: ['src/**/*.{ts,tsx}']
+      }),
+      json(),
       postcss({ minimize: true }),
       terser({
         compress: {
           drop_console: true,
-          drop_debugger: true,
+          drop_debugger: true
         },
         output: {
-          comments: false,
-        },
+          comments: false
+        }
       }),
-      image(),
-    ],
-  },
-];
+      image()
+    ]
+  }
+]
