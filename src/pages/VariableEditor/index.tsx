8debd186d76ba72c3b16274ad18ba03d53ca6574
foxxy <veselovnk@txix.ru>
2025-03-10 16:11:17 +0300

    MSP-5011 create VariableEditor

---
+++ src/pages/VariableEditor/index.tsx
@@ -0,0 +1,127 @@
+import { Spin, message } from 'antd'
+import { AxiosError } from 'axios'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+
+import VariableEditorForm from '@components/VariablesComponents/VariableEditorForm'
+import VariableEditorHeader from '@components/VariablesComponents/VariableEditorHeader'
+
+import { handleVariableApiError } from '@utils/variableUtils.ts'
+
+import variablesApi from '@services/variablesService.ts'
+
+import { routePaths } from '@routes/routePaths'
+
+import { MSPError } from '@type/index.ts'
+
+import './variable-editor.scss'
+
+const VariableEditor = () => {
+  const navigate = useNavigate()
+  const { id = '' } = useParams()
+  const isNewVariable = id === 'new'
+
+  const {
+    data: variable,
+    isLoading,
+    isError,
+    error
+  } = variablesApi.useGetVariableByNameQuery(id, { skip: isNewVariable })
+  const { control, watch, reset, handleSubmit, setError } =
+    useForm<VariableType>({
+      defaultValues: { type: 'TEXT' }
+    })
+  const typeWatch = watch('type')
+
+  const [createVariable, { isLoading: isCreateMethodLoading }] =
+    variablesApi.useCreateVariableMutation()
+  const [updateVariable, { isLoading: isUpdateMethodLoading }] =
+    variablesApi.useUpdateVariableMutation()
+
+  const isSomeMethodLoading = isUpdateMethodLoading || isCreateMethodLoading
+
+  useEffect(() => {
+    if (variable) reset(variable)
+  }, [reset, variable])
+
+  if (isLoading) {
+    return <Spin fullscreen />
+  }
+  if (isError) {
+    handleVariableApiError(
+      error as AxiosError<MSPError>,
+      'Произошла ошибка получения переменной',
+      { message }
+    )
+    return <Navigate to={routePaths.error} />
+  }
+
+  const goToVariablePage = () => navigate(routePaths.variables)
+
+  const handleCreateVariable = async (formData: VariableType) => {
+    const sendData: NewVariableType = {
+      description: formData.description,
+      name: formData.name,
+      value: formData.value,
+      type: formData.type
+    }
+    createVariable(sendData)
+      .unwrap()
+      .then(() => {
+        message.success('Переменная успешно создана')
+        goToVariablePage()
+      })
+      .catch((error: AxiosError<MSPError>) => {
+        handleVariableApiError(error, 'Ошибка при создании переменной', {
+          setError,
+          message
+        })
+      })
+  }
+
+  const handleUpdateVariable = async (formData: VariableType) => {
+    const sendData: UpdateVariableType = {
+      description: formData.description,
+      name: formData.name,
+      value: formData.value
+    }
+    updateVariable(sendData)
+      .unwrap()
+      .then(() => {
+        message.success('Переменная успешно обновлена!')
+        goToVariablePage()
+      })
+      .catch((error: AxiosError<MSPError>) => {
+        handleVariableApiError(error, 'Ошибка при обновлении переменной', {
+          setError,
+          message
+        })
+      })
+  }
+
+  const handleSubmitForm = (formData: VariableType) => {
+    if (isNewVariable) {
+      handleCreateVariable(formData)
+    } else {
+      handleUpdateVariable(formData)
+    }
+  }
+
+  return (
+    <form className="variable-editor">
+      <VariableEditorHeader
+        isLoading={isSomeMethodLoading}
+        title={isNewVariable ? 'Новая переменная' : variable!.name}
+        onSubmit={handleSubmit(handleSubmitForm)}
+      />
+      <VariableEditorForm
+        control={control}
+        isNewVariable={isNewVariable}
+        typeWatch={typeWatch}
+      />
+    </form>
+  )
+}
+
+export default VariableEditor
