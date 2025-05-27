96b2c7866eb2a895fde9b10d4ee8f9d1e23ee376
txix <txix@txix.ru>
2025-05-27 12:14:39 +0300

    2025-05-27 12:14:39: merge [main:b2bcc5699ef09b40f6a8c52be4fd9a1f8e38ea37];

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,4 +1,9 @@
+import { theme } from 'antd'
+import { useMemo } from 'react'
+
 import { HeaderPropsType } from '@widgets/Header/header.type.ts'
+
+import { getContrastTextColor } from '@utils/styleUtils.ts'
 
 import { useAppSelector } from '@hooks/redux.ts'
 
@@ -8,17 +13,32 @@ const Header = ({ collapsed }: HeaderPropsType) => {
   const {
     ui: { name, primaryColor }
   } = useAppSelector((state) => state.UIReducer)
+  const { useToken } = theme
+  const { token } = useToken()
+
+  const safePrimaryColor = primaryColor || token.colorBgLayout
+
+  const textColor = useMemo(
+    () => getContrastTextColor(safePrimaryColor),
+    [safePrimaryColor]
+  )
+
+  const collapsedClassName = collapsed ? 'header__text-closed' : 'header__text'
 
   return (
     <section
       className="header"
       style={{
-        backgroundColor: primaryColor
+        backgroundColor: safePrimaryColor,
+        color: textColor
       }}
     >
-      <span className={collapsed ? 'header__text-closed' : 'header__text'}>
-        {name}
-      </span>
+      <div className={`header__container ${collapsedClassName}`}>
+        <span className="header__container__project-name">{name}</span>
+        <span className="header__container__project-version">
+          v{window.AppVersion}
+        </span>
+      </div>
     </section>
   )
 }
