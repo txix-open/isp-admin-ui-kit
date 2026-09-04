505746a61d128f5ae66f7f80c46ce327405d34f0
Anastasiia <anufrievaad@txix.ru>
2026-09-04 17:08:28 +0300

    MSP-8174 add oAuth authorization

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -10,25 +10,54 @@ import { LocalStorage } from '@utils/localStorageUtils'
 import { routePaths } from '@routes/routePaths'
 
 const useLogout = () => {
-  const { logout, isLoading } = useAuth()
+  const { logout, isLoading, oAuthLogout } = useAuth()
   const navigate = useNavigate()
   const headerName = LocalStorage.get(localStorageKeys.HEADER_NAME)
+  const isOAuthLogin = LocalStorage.get(localStorageKeys.OAUTH_LOGIN)
 
-  const logoutUser = () =>
-    logout(apiPaths.logout, {
-      'X-APPLICATION-TOKEN': getConfigProperty(
-        'APP_TOKEN',
-        import.meta.env.VITE_APP_TOKEN
-      ),
-      [headerName]: LocalStorage.get(localStorageKeys.USER_TOKEN)
-    })
-      .then(() => {
-        LocalStorage.remove(localStorageKeys.USER_TOKEN)
-        LocalStorage.remove(localStorageKeys.HEADER_NAME)
-        sessionStorage.clear()
-        navigate(routePaths.login, { replace: true })
+  const clearAuthState = () => {
+    LocalStorage.remove(localStorageKeys.USER_TOKEN)
+    LocalStorage.remove(localStorageKeys.HEADER_NAME)
+    sessionStorage.clear()
+  }
+
+  const logoutUser = () => {
+    if (isOAuthLogin) {
+      oAuthLogout(
+        apiPaths.loginOAuth,
+        {
+          clientName: import.meta.env.VITE_CLIENT_NAME
+        },
+        {
+          'Content-Type': 'application/json',
+          'X-APPLICATION-TOKEN': getConfigProperty(
+            'APP_TOKEN',
+            import.meta.env.VITE_APP_TOKEN
+          ),
+          [headerName]: LocalStorage.get(localStorageKeys.USER_TOKEN)
+        }
+      )
+        .then((data) => {
+          LocalStorage.remove(localStorageKeys.OAUTH_LOGIN)
+          clearAuthState()
+          window.location.href = data.logoutUrl
+        })
+        .catch(() => {})
+    } else {
+      logout(apiPaths.logout, {
+        'X-APPLICATION-TOKEN': getConfigProperty(
+          'APP_TOKEN',
+          import.meta.env.VITE_APP_TOKEN
+        ),
+        [headerName]: LocalStorage.get(localStorageKeys.USER_TOKEN)
       })
-      .catch(() => {})
+        .then(() => {
+          clearAuthState()
+          navigate(routePaths.login, { replace: true })
+        })
+        .catch(() => {})
+    }
+  }
   return { isLoading, logoutUser }
 }
 
